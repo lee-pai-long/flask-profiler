52e3b4f1c96b765dbc90b25053d6ce48af043ea8
lpl <lee-pai-long@users.noreply.github.com>
2017-08-07 20:26:08 +0200

    Remove useless nesting
    (and correct convention on inline comments)

--- flask_profiler/storage/__init__.py
+++ flask_profiler/storage/__init__.py
@@ -24,36 +24,36 @@ def cwd_in_path():
 
 def getCollection(conf):
     engine = conf.get("engine", "")
-    # TODO: Remove useless nesting (when return no need to else)
+
     if engine.lower() == "mongodb":
         from .mongo import Mongo
         return Mongo(conf)
-    elif engine.lower() == "sqlite":
+
+    if engine.lower() == "sqlite":
         from .sqlite import Sqlite
         return Sqlite(conf)
-    else:
-        try:
-            parts = engine.split('.')
-            # NOTE: flake8 E261 (two space before inline comment)
-            if len(parts) < 1: # engine must have at least module name and class
-                raise ImportError
 
-            module_name = '.'.join(parts[:-1])
-            # NOTE: No need to use a k for class when appending the variable name
-            klass_name = parts[-1]
+    try:
+        parts = engine.split('.')
+        if len(parts) < 1:  # engine must have at least module name and class
+            raise ImportError
 
-            # we need to make sure that it will be able to find module in your
-            # project directory
-            with cwd_in_path():
-                module = importlib.import_module(module_name)
+        module_name = '.'.join(parts[:-1])
+        # NOTE: No need to use a k for class when appending the variable name
+        klass_name = parts[-1]
 
-            storage = getattr(module, klass_name)
-            if not issubclass(storage, BaseStorage):
-                raise ImportError
+        # we need to make sure that it will be able to find module in your
+        # project directory
+        with cwd_in_path():
+            module = importlib.import_module(module_name)
 
-        except ImportError:
-            raise ValueError(
-                ("flask-profiler requires a valid storage engine but it is"
-                    " missing or wrong. provided engine: {}".format(engine)))
+        storage = getattr(module, klass_name)
+        if not issubclass(storage, BaseStorage):
+            raise ImportError
+
+    except ImportError:
+        raise ValueError(
+            ("flask-profiler requires a valid storage engine but it is"
+                " missing or wrong. provided engine: {}".format(engine)))
 
         return storage(conf)
